c23ee436c9033cf74422f8026e7649f18156584f
Nima <nims.844@gmail.com>
2024-12-08 23:44:44 +0330

    finished the product controller logic

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -47,7 +47,7 @@ const addProduct = async (req, res) => {
 };
 
 // function for list product
-const listProducts = async () => {
+const listProducts = async (req, res) => {
     try {
         const products = await productModel.find({});
         res.json({ success: true, products })
@@ -58,13 +58,26 @@ const listProducts = async () => {
 };
 
 // function for removing product
-const removeProduct = async () => {
-
+const removeProduct = async (req, res) => {
+    try {
+        await productModel.findByIdAndDelete(req.body.id);
+        res.json({ success: true, message: "Product Removed" })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 };
 
 // function for single product info
-const singleProduct = async () => {
-
+const singleProduct = async (req, res) => {
+    try {
+        const { productId } = req.body;
+        const product = await productModel.findById(productId);
+        res.json({ success: true, product });
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message });
+    }
 };
 
 export { addProduct, listProducts, removeProduct, singleProduct };
