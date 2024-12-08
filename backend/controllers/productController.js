a33eebcc8c99405761ffa5c176f235473521cf3b
Nima <nims.844@gmail.com>
2024-12-08 23:29:06 +0330

    added the logic to add product in prodcut controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,70 @@
+import { v2 as cloudinary } from 'cloudinary';
+import productModel from '../models/productModel.js'
+
+// function for add product
+const addProduct = async (req, res) => {
+    try {
+        const { name, description, price, category, subCategory, sizes, bestSeller } = req.body;
+
+        // 6:45 images
+        const image1 = req.files.image1 && req.files.image1[0];
+        const image2 = req.files.image2 && req.files.image2[0];
+        const image3 = req.files.image3 && req.files.image3[0];
+        const image4 = req.files.image4 && req.files.image4[0];
+
+        const images = [image1, image2, image3, image4].filter(item => item != undefined);
+
+        let imagesUrl = await Promise.all(
+            images.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path, { resource_type: 'image' });
+                return result.secure_url;
+            })
+        );
+
+        const productData = {
+            name,
+            description,
+            category,
+            price: Number(price),
+            subCategory,
+            bestSeller: bestSeller === "true" ? true : false,
+            sizes: JSON.parse(sizes),
+            image: imagesUrl,
+            date: Date.now(),
+        };
+
+        console.log(productData);
+
+        const product = new productModel(productData);
+        await product.save();
+
+        res.json({ success: true, message: "Product Added" });
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+};
+
+// function for list product
+const listProducts = async () => {
+    try {
+        const products = await productModel.find({});
+        res.json({ success: true, products })
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false, message: error.message })
+    }
+};
+
+// function for removing product
+const removeProduct = async () => {
+
+};
+
+// function for single product info
+const singleProduct = async () => {
+
+};
+
+export { addProduct, listProducts, removeProduct, singleProduct };
