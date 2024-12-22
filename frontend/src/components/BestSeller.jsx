128842542fd02a622c38b800828ddb676ca957d5
Nima <nims.844@gmail.com>
2024-12-22 19:59:20 +0330

    added the products to Database & the logic to display it in the frontend

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -9,34 +9,34 @@ const BestSeller = () => {
     const [bestSeller, setBestSeller] = useState([]);
 
     useEffect(() => {
-        const bestProduct = products.filter((item) => item.bestseller);
+        const bestProduct = products.filter((item) => item.bestSeller);
         setBestSeller(bestProduct.slice(0, 5));
-    },[]);
-    
-  return (
+    }, [products]);
 
-    <div className='my-10'>
-        
-        <div className='text-center text-3xl py-8'>
-            <Title text1={'BEST'} text2={'SELLERS'}/>
+    return (
 
-            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam deserunt explicabo repudiandae, eos dicta recusandae provident delectus debitis voluptas.
-            </p>
+        <div className='my-10'>
+
+            <div className='text-center text-3xl py-8'>
+                <Title text1={'BEST'} text2={'SELLERS'} />
+
+                <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam deserunt explicabo repudiandae, eos dicta recusandae provident delectus debitis voluptas.
+                </p>
+
+            </div>
+
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+                {
+                    bestSeller.map((item, index) => (
+                        <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} />
+                    ))
+                }
+            </div>
 
         </div>
 
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-            {
-                bestSeller.map((item, index) => (
-                    <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price}/>
-                ))
-            }
-        </div>
-
-    </div>
-
-  )
+    )
 }
 
 export default BestSeller
