5845f62dbb66c72df36b95dcc918f727b4db2cf3
Nima <nims.844@gmail.com>
2025-01-13 00:18:12 +0330

    added finished the order page for admin panel & finished order logic for the project :)

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -41,7 +41,13 @@ const placeOrderRazorpay = async (req, res) => {
 
 // All orders data for admin panel
 const allOrders = async (req, res) => {
-
+    try {
+        const orders = await orderModel.find({});
+        res.json({ success: true, orders });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
 }
 
 // user order data for frontend
@@ -59,7 +65,16 @@ const userOrders = async (req, res) => {
 
 // update order status
 const updateStatus = async (req, res) => {
+    try {
+        const { orderId, status } = req.body;
 
+        await orderModel.findByIdAndUpdate(orderId, { status });
+
+        res.json({ success: true, message: 'Status Updated' });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
 }
 
 export { placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus };
